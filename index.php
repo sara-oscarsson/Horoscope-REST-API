60e2198717a9e5af40b5166e0e0478acdc1114c1
Sara Oscarsson <oscarsson.sara@hotmail.com>
2020-12-01 09:23:09 +0100

    style

--- index.php
+++ index.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horoscope</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <header>
+    <h1>Horoscope</h1>
+    </header>
+    <div class="inputContainer">
+        <label for="">Date of birth:</label>
+        <input type="text" name="" id="birthDate" placeholder="mmdd">
+        <button id="saveBtn">Save</button>
+        <button id="updateBtn">Update</button>
+        <button id="deleteBtn">Delete</button>
+    </div>
+    <main>
+        <div class="horoscopeContainer">
+            <h2 id="horoscopeTitle">Stjärntecken</h2>
+            <div id="txt">Horoskop</div>
+    
+        </div>
+    </main>
     
 </body>
 </html>
